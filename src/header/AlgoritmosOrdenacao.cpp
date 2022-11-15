1e7311d198b5440bab5c6d5b92637f27ae429a77
0ket <ketleen@ice.ufjf.br>
2022-11-15 13:42:45 -0300

    iniciando encaps do metodo time sort

--- src/header/AlgoritmosOrdenacao.cpp
+++ src/header/AlgoritmosOrdenacao.cpp
@@ -64,18 +64,22 @@ void AlgoritmosOrdenacao::quickSort(ProductReview* vet, int size) {
     arq.temp(resultado);
 }
 
-void AlgoritmosOrdenacao::merge(ProductReview* vet, ProductReview* aux, int inicio, int meio, int fim, int& comparacoes, int& movimentacoes) {
+void AlgoritmosOrdenacao::merge(ProductReview* vet, ProductReview* aux, int inicio, int meio, int fim, int& comparacoes, int& movimentacoes) 
+{                                                                               
     int i = inicio;
     int j = meio + 1;
     int k = 0;
 
-    while (i <= meio && j <= fim) {
+    while (i <= meio && j <= fim) 
+    {                                  
         // comparando valor se vet[i] é menor
-        if (vet[i].getUserId().compare(vet[j].getUserId()) <= 0) {
+        if (vet[i].getUserId().compare(vet[j].getUserId()) <= 0) 
+        {                                                                 
             aux[k] = vet[i];
             i++;
 
-        } else {
+        } else 
+        {                                                                                                   
             aux[k] = vet[j];
             j++;
         }
@@ -84,28 +88,34 @@ void AlgoritmosOrdenacao::merge(ProductReview* vet, ProductReview* aux, int inic
         k++;
     }
 
-    while (i <= meio) {
+    while (i <= meio) 
+    {                                                          
         aux[k] = vet[i];
         i++;
         k++;
         movimentacoes++;
-    }
-    while (j <= fim) {
+    }                            
+
+    while (j <= fim) 
+    {                                         
         aux[k] = vet[j];
         j++;
         k++;
         movimentacoes++;
     }
-    for (k = 0; k < fim - inicio + 1; k++) {
+                          
+    for (k = 0; k < fim - inicio + 1; k++) 
+    {                                        
         vet[inicio + k] = aux[k];
         movimentacoes++;
     }
 }
 
-void AlgoritmosOrdenacao::mergeSortEncaps(ProductReview* vet, ProductReview* aux, int inicio, int fim, int& comparacoes, int& movimentacoes) {
-    if (inicio < fim) {
+void AlgoritmosOrdenacao::mergeSortEncaps(ProductReview* vet, ProductReview* aux, int inicio, int fim, int& comparacoes, int& movimentacoes)
+{                                                                                                                    
+    if (inicio < fim) 
+    {                                                                                
         int meio = (inicio + fim) / 2;
-
         // Dividindo a primeira metade
         mergeSortEncaps(vet, aux, inicio, meio, comparacoes, movimentacoes);
         // Dividindo a segunda metade
@@ -115,7 +125,8 @@ void AlgoritmosOrdenacao::mergeSortEncaps(ProductReview* vet, ProductReview* aux
     }
 }
 
-void AlgoritmosOrdenacao::mergeSort(ProductReview* vet, int size) {
+void AlgoritmosOrdenacao::mergeSort(ProductReview* vet, int size) 
+{                                               
     ManipulandoArquivo arq;
 
     ProductReview* aux = new ProductReview[size];
@@ -133,9 +144,10 @@ void AlgoritmosOrdenacao::mergeSort(ProductReview* vet, int size) {
     this->resultado.setComparacao(comparacoes);
     this->resultado.setMovimentacao(movimentacoes);
     arq.temp(resultado);
-}
+}                                                
 
-void AlgoritmosOrdenacao::insertionSort(ProductReview* vetor, int esquerda, int direita, int& comparacoes, int& movimentacoes) {
+void AlgoritmosOrdenacao::insertionSort(ProductReview* vetor, int esquerda, int direita, int& comparacoes, int& movimentacoes)
+{                                                                                                                                                                                                    
     for (int i = esquerda + 1; i <= direita; i++) {
         string temp = vetor[i].getUserId();
         int j = i - 1;
@@ -150,18 +162,15 @@ void AlgoritmosOrdenacao::insertionSort(ProductReview* vetor, int esquerda, int 
         movimentacoes++;
     }
 }
+                                                       
 const int RUN = 32;
-void AlgoritmosOrdenacao::timSort(ProductReview* vet, int size) {
-    ManipulandoArquivo arq;
-    int comparacoes = 0, movimentacoes = 0;
 
-    high_resolution_clock::time_point inicio = high_resolution_clock::now();
+void AlgoritmosOrdenacao::timeSortEncaps(ProductReview* vet, ProductReview* aux, int size, int& comparacoes, int& movimentacoes)
+{                   
 
     for (int i = 0; i < size; i += RUN) {
         insertionSort(vet, i, min((i + RUN - 1), (size - 1)), comparacoes, movimentacoes);
     }
-    
-    ProductReview* aux = new ProductReview[size];
     for (int tamanho = RUN; tamanho < tamanho; tamanho = 2 * tamanho) {
         for (int esquerda = 0; esquerda < tamanho; esquerda += 2 * tamanho) {
             int meio = esquerda + tamanho - 1;
@@ -177,13 +186,24 @@ void AlgoritmosOrdenacao::timSort(ProductReview* vet, int size) {
         }
     }
 
+}
+void AlgoritmosOrdenacao::timSort(ProductReview* vet, int size) {
+
+    ManipulandoArquivo arq;
+    ProductReview* aux = new ProductReview[size];
+    int comparacoes = 0, movimentacoes = 0;
+                                                   
+    high_resolution_clock::time_point inicio = high_resolution_clock::now();
+
+    timeSortEncaps(vet, aux, size, comparacoes, movimentacoes);
+
     high_resolution_clock::time_point fim = high_resolution_clock::now();
 
     double time = duration_cast<duration<double>>(fim - inicio).count();
 
-    this->resultado.setTempoExecucao(time);
-    this->resultado.setComparacao(comparacoes);
-    this->resultado.setMovimentacao(movimentacoes);
+    this->resultado.setTempoExecucao(time);                   
+    this->resultado.setComparacao(comparacoes);                   
+    this->resultado.setMovimentacao(movimentacoes);              
 
     arq.temp(resultado);
 }
