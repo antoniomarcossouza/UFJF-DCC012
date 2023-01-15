af39da27ce0221d6d2ee9697ec39e91e6961d823
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2023-01-15 14:43:07 -0300

    writing docs

--- src/header/CompressaoHuffman.cpp
+++ src/header/CompressaoHuffman.cpp
@@ -4,14 +4,21 @@
 #include <queue>
 
 /*
-Parâmetros: 
+    Parâmetros:
+    raiz: ponto inicial da árvore de Huffman
+    str: string vazia que é usada como prefixo para a codificação de Huffman
+    huffmanCode: mapa que armazena a codificação Huffman para cada caractere
 
+    Código:
+    Verifica se a raiz é nula e, se for, retorna imediatamente.
+    Se a raiz não for nula, verifica se o nó atual tem um caractere diferente de '\0', que indica que é uma folha,
+    e, se tiver, adiciona o caractere e o código Huffman correspondente ao mapa huffmanCode.
 
-Variáveis:
+    Em seguida, a função chama a si mesma recursivamente para o filho esquerdo e para o filho direito,
+    passando str + "0" e str + "1" como o novo prefixo para a codificação Huffman, respectivamente.
+    Isso garante que a codificação Huffman seja construída de acordo com a árvore de Huffman correta.
 
-
-Código:
-
+    Por fim, toda a codificação Huffman para cada caractere estará armazenada no mapa huffmanCode.
 */
 void CompressaoHuffman::buildCodes(Node *raiz, string str, unordered_map<char, string> &huffmanCode) {
     if (raiz == nullptr) {
@@ -25,23 +32,28 @@ void CompressaoHuffman::buildCodes(Node *raiz, string str, unordered_map<char, s
 }
 
 /*
-Parâmetros: 
+    Parâmetros:
+    texto: texto a ser comprimido
+    huffmanCode: mapa que armazena a codificação Huffman para cada caractere
+    filaPrioridade: fila de prioridade usada para construir a árvore de Huffman
 
+    Código:
+    A função começa construindo uma tabela de frequência para cada caractere no texto e
+    adiciona cada caractere e sua frequência à fila de prioridade.
 
-Variáveis:
+    A função então constrói a árvore de Huffman removendo os dois nós de menor frequência da fila de
+    prioridade, criando um novo nó pai com a soma das frequências dos dois nós removidos e adicionando-o
+    de volta à fila. Isso é repetido até que a fila tenha apenas um nó, que é a raiz da árvore de Huffman.
 
-
-Código:
-
+    Por fim, a função chama a função buildCodes passando a raiz da árvore de Huffman, uma string vazia e
+    o mapa da codificação Huffman como parâmetros, a fim de construir a codificação Huffman para cada caractere.
 */
 void CompressaoHuffman::buildHuffmanTree(string texto, unordered_map<char, string> &huffmanCode, priority_queue<Node *, vector<Node *>, Compare> &filaPrioridade) {
-    // Build frequency table
     unordered_map<char, int> freqTable;
     for (char c : texto) {
         freqTable[c]++;
     }
 
-    // Build priority queue
     for (auto pair : freqTable) {
         filaPrioridade.push(new Node(pair.first, pair.second));
     }
@@ -61,19 +73,19 @@ void CompressaoHuffman::buildHuffmanTree(string texto, unordered_map<char, strin
 }
 
 /*
-Parâmetros: 
-texto: texto a ser comprimido
-huffmanCode: mapa que contém os códigos Huffman para cada caractere
+    Parâmetros:
+    texto: texto a ser comprimido
+    huffmanCode: mapa que armazena a codificação Huffman para cada caractere
 
-Variáveis:
-textoCodificado: será preenchida com o texto comprimido
+    Variáveis:
+    textoCodificado: será preenchida com o texto comprimido
 
-Código:
-Loop que percorre cada caractere do texto. 
-Para cada caractere, o código adiciona o código Huffman correspondente,
-que é recuperado do huffmanCode, ao textoCodificado.
+    Código:
+    Loop que percorre cada caractere do texto.
+    Para cada caractere, o código adiciona o código Huffman correspondente,
+    que é recuperado do huffmanCode, ao textoCodificado.
 
-Ao fim do loop, a função retorna o texto comprimido.
+    Ao fim do loop, a função retorna o texto comprimido.
 */
 string CompressaoHuffman::comprimir(string texto, unordered_map<char, string> &huffmanCode) {
     string textoCodificado = "";
@@ -84,23 +96,23 @@ string CompressaoHuffman::comprimir(string texto, unordered_map<char, string> &h
 }
 
 /*
-Parâmetros: 
-textoCodificado: texto a ser descomprimido
-raiz: ponto inicial da árvore de Huffman usada para a codificação
+    Parâmetros:
+    textoCodificado: texto a ser descomprimido
+    raiz: ponto inicial da árvore de Huffman usada para a codificação
 
-Variáveis:
-textoDecodificado: será preenchida com o texto descomprimido
-current: aponta para o nó atual, inicialmente para a raiz da árvore de Huffman
+    Variáveis:
+    textoDecodificado: será preenchida com o texto descomprimido
+    current: aponta para o nó atual, inicialmente para a raiz da árvore de Huffman
 
-Código:
-Loop que percorre os caracteres do texto codificado. 
-Para cada caractere, o código verifica se é igual a '0' ou '1', 
-e move o ponteiro current para o filho esquerdo ou direito da árvore, respectivamente.
+    Código:
+    Loop que percorre os caracteres do texto codificado.
+    Para cada caractere, o código verifica se é igual a '0' ou '1',
+    e move o ponteiro current para o filho esquerdo ou direito da árvore, respectivamente.
 
-Se o nó atual tem um caractere diferente de '\0', que indica que é uma folha, ele é adicionado 
-ao textoDecodificado e o ponteiro current é apontado para a raiz novamente.
+    Se o nó atual tem um caractere diferente de '\0', que indica que é uma folha, ele é adicionado
+    ao textoDecodificado e o ponteiro current é apontado para a raiz novamente.
 
-Ao fim do loop, a função retorna o texto descomprimido.
+    Ao fim do loop, a função retorna o texto descomprimido.
 */
 string CompressaoHuffman::descomprimir(string textoCodificado, Node *raiz) {
     string textoDecodificado = "";
